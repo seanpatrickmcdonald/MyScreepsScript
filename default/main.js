beb03fc8c66fd545e7b45a137d84ca6dbf946bea
Sean McDonald <spmcdonald@nevada.unr.edu>
2019-03-02 05:00:27 -0800

    Removed cpu bucket visual

--- default/main.js
+++ default/main.js
@@ -2,12 +2,10 @@ var room = require('room')
 
 module.exports.loop = function(){
     
-    
     for(var name in Game.rooms){
         room.update(Game.rooms[name])
         
     }
     
     
-    new RoomVisual().text(Game.cpu.bucket, 1, 0);
 };
