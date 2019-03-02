5fe67ed0aca08f0abb528ffd6a25cad3b1ed3b47
Sean McDonald <spmcdonald@nevada.unr.edu>
2019-03-02 05:58:35 -0800

    write findTask function

--- default/creep.harvester.js
+++ default/creep.harvester.js
@@ -1,12 +1,41 @@
 
-var creep.harvester = {
+var harvester = {
     run : function(creep){
         
     },
     
     findTask: function(creep){
-        if(creep.room.memory.spawns[0])
+        //Reset task
+        creep.memory.task = null;
+    
+        //Search for Spawn below capacity
+        for(var index = 0; index < creep.room.memory.spawns.length; index++){
+            if(Game.getObjectById(creep.room.memory.spawns[index]).energy < SPAWN_ENERGY_CAPACITY){
+                creep.memory.task = creep.room.memory.spawns[index];
+                creep.memory.taskAction = 'FILL_SPAWN';
+                return;
+            }
+        }
+        
+        //Search for Extension below capacity
+        const extension = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, 
+            {filter: function(object){
+                return structureType == STRUCTURE_EXTENSION && object.energy < object.energyCapacity;
+            }});
+        
+        if(extension){ 
+            creep.memory.task = extension.id; 
+            creep.memory.taskAction = 'FILL_SPAWN'; 
+            return;
+        }
+        
+        //Search for Container or Storage below capacity
+        const storage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, 
+            {filter: function(object){
+                return (structureType == STRUCTURE_CONTAINER || structureType == STRUCTURE_STORAGE) && 
+                    object.store[RESOURCE_ENERGY] < object.storeCapacity;
+            }});
     }
 };
 
-module.exports.creep.harvester = creep.harvester
+module.exports.harvester = harvester
